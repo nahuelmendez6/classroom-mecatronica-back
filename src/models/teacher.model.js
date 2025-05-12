5e20bfbdd7c30023e44cfdc8c0180e62e3220f8b
nahuelmendez6 <mendezmaturano6@gmail.com>
2025-05-12 16:15:38 -0300

    feat(auth): implementacion de modelo Teacher

---
+++ src/models/teacher.model.js
@@ -0,0 +1,46 @@
+import { pool } from "../../config/database";
+import bcrypt from "bcryptjs/dist/bcrypt";
+
+/**
+ * Clase que maneja las operaciones de base de datos relacionadas con los profesores
+ */
+class Teacher {
+
+
+    static async create(teacherData) {
+        const connection = await pool.getConnection();
+
+        try {
+            await connection.beginTransaction();
+
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(teacherData.password, salt);
+
+            const [userResult] = await connection.query(
+                'INSERT INTO user (email, password, name, lastname, id_role) VALUES (?, ?, ?, ?, 2)',
+                [teacherData.email, hashedPassword, teacherData.name, teacherData.lastname]
+            )
+
+            const [teacherResult] = await connection.query(
+                'INSERT INTO teacher (id_user) VALUES (?)',
+                [userResult.insertId]
+            )
+
+            await connection.commit();
+
+            return {
+                id: userResult.insertId,
+                ...teacherData,
+                password: undefined // No devolver la contraseña
+            };
+
+        } catch (error) {
+            await connection.rollback();
+            throw error;
+        } finally {
+            connection.release();
+        }
+
+    }
+
+}
